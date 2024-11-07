71ed04032a1a35e331966dcf34a0a1320636cee7
atb-brown <austin.brown@wealthcounsel.com>
2024-11-07 11:14:33 -0600

    Refactor TestDataRunner
    This commit refactors the TestDataRunner to improve it in a few aspects.
    
     1. The expected json format is now closer to the type of test case data provided by leetcode
     2. Test cases that have multiple input parameters are now being handled.
     3. Better exception handling/messaging for missing test JSON keys.

--- app/src/test/kotlin/org/hash/HashMapTest.kt
+++ app/src/test/kotlin/org/hash/HashMapTest.kt
@@ -1,5 +1,6 @@
 package org.hash
 
+import org.util.TestDataRunner
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -16,5 +17,47 @@ class HashMapTest {
         assertEquals(1, myMap.get(2))
         myMap.remove(2)
         assertEquals(-1, myMap.get(2))
+    }
+
+    @Test
+    fun largeTest() {
+        val jsonReader = TestDataRunner("/org/hash/hashMapTestData.json")
+
+        val myMap = TroubleshootingHashMap()
+        jsonReader.forEach { operation, params ->
+            val key = params[0].toInt()
+            val value =
+                if ("put".equals(operation)) {
+                    params[1].toInt()
+                } else {
+                    0
+                }
+            return@forEach doOperation(myMap, operation, key, value)
+        }
+    }
+
+    private fun doOperation(
+        myMap: TroubleshootingHashMap,
+        operation: String,
+        key: Int,
+        value: Int,
+    ): String {
+        return when (operation) {
+            "put" -> {
+                myMap.put(key, value)
+                "null"
+            }
+
+            "get" -> {
+                myMap.get(key).toString()
+            }
+
+            "remove" -> {
+                myMap.remove(key)
+                "null"
+            }
+
+            else -> error("Unrecognized operation.")
+        }
     }
 }
